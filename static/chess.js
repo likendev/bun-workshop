8082309f1a3b327549d267e46680976430ee5925
Liken Tan <likendev@gmail.com>
2023-11-04 15:13:10 +0800

    feat: import chessboard dependencies

--- static/chess.js
+++ static/chess.js
@@ -6,3 +6,18 @@ const gameId = body.dataset.gameId;
 const orientation = body.dataset.orientation;
 
 console.info(`gameId: ${gameId}, orientation: ${orientation}`);
+
+// Handle onDrop
+const onDrop = (src, dst,piece) => {
+    console.info(`src=${src}, dst=${dst}, piece=${piece}`);
+}
+// Create a chess configuration
+const config = {
+    draggable: true,
+    position: 'start',
+    orientation,
+    onDrop
+}
+
+// Create instance of chess
+const chess = Chessboard('chess', config)
